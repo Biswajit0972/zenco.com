46f1a7f67bc8576015cd7179a11ddce1cef01b5a
Biswajit Das <workbiswajit0981@gmail.com>
2025-12-02 20:52:34 +0530

    Fix: format Hero component usage for improved code readability

--- app/page.tsx
+++ app/page.tsx
@@ -2,9 +2,8 @@ import Hero from "./_components/Hero";
 
 export default function Home() {
   return (
-   <section className="relative  w-full overflow-x-hidden overflow-y-auto ">
-       <Hero/>
-
-   </section>
+    <section className="relative  w-full overflow-x-hidden overflow-y-auto ">
+      <Hero />
+    </section>
   );
 }
