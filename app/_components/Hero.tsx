a12445382762efe373e224ec25c227b8a2213cd5
Biswajit Das <workbiswajit0981@gmail.com>
2025-12-02 15:27:15 +0530

    Refactor Hero component layout and update background image

--- app/_components/Hero.tsx
+++ app/_components/Hero.tsx
@@ -2,36 +2,31 @@ import Image from "next/image";
 import HeroTextHighlight from "@/app/_components/HeroTextHighlight";
 
 const Hero = () => {
-    return (
-        <div id="hero">
-            <div className="poster">
-                <Image src="/images/bg1.jpg" alt="bg" fill className="object-cover"/>
-            </div>
-            <div className="text-box">
-                <div className="md:text-center">
-                    <h1 className="font-mono font-[500]">
-                        Creating
-                        <HeroTextHighlight>Awesome</HeroTextHighlight>
-                    </h1>
-                </div>
-                <div>
-                    <h1 className="font-mono font-[500] md:text-center">
-                        and digital
-                    </h1>
-                </div>
-                <div className="h-30 overflow-hidden">
-                    <div className="h-full w-full sm:w-[55%] xl:w-[40%] absolute sm:right-18 md:right-15 ">
-                        <h1 className=" flex items-center">
-                            <HeroTextHighlight>Solutions</HeroTextHighlight>
-                        </h1>
-                    </div>
-                </div>
-            </div>
+  return (
+    <div id="hero">
+      <div className="poster">
+        <Image src="/images/bg2.png" alt="bg" fill className="object-cover" />
+      </div>
+      <div className="text-box center ">
+        <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2  px-5  md:w-[75%] lg:w-[85%]  xl:w-[65%] xl:px-4 py-5 max-w-5xl">
+          <div className="text-left  xl:h-44 overflow-hidden ">
+            <h1 className=" font-bold">
+              Creating
+              <HeroTextHighlight className="mt-5 ml-3">brand</HeroTextHighlight>
+            </h1>
+          </div>
+          <div className="relative h-20 sm:h-24  xl:h-36">
+            <h1 className="font-bold ">and digital</h1>
+          </div>
+          <div className="h-22 overflow-hidden  relative mt-3 xl:h-38 text-center sm:h-24 md:h-28">
+            <h1 className=" xl:absolute xl:right-0 ">
+              <HeroTextHighlight>Solutions</HeroTextHighlight>
+            </h1>
+          </div>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default Hero;
-
-
-
