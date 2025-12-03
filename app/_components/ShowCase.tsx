b9edade9af4851e1ceadb2abaa01ec64ba35df7b
Biswajit Das <workbiswajit0981@gmail.com>
2025-12-03 19:42:41 +0530

    Add: `Showcase section in landing page`

---
+++ app/_components/ShowCase.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+
+const ShowCase = () => {
+    return (
+        <div className="showcase">ShowCase</div>
+    )
+}
+export default ShowCase
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 import Hero from "./_components/Hero";
+import ShowCase from "@/app/_components/ShowCase";
 
 export default function Home() {
-  return (
-    <section className="relative  w-full overflow-x-hidden overflow-y-auto ">
-      <Hero />
-    </section>
-  );
+    return (
+        <section className="relative  w-full overflow-x-hidden overflow-y-auto ">
+            <Hero/>
+            <ShowCase/>
+        </section>
+    );
 }
