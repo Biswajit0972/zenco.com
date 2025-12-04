8ec2f2f3367b3debac6e69c332a6158e17881616
roni <codingpro0981@gmail.com>
2025-12-04 20:02:04 +0530

    Enhance: Add active state and onClick handler to `NavigationLink` component

--- app/_components/NavigationLink.tsx
+++ app/_components/NavigationLink.tsx
@@ -1,15 +1,21 @@
-"use client";
-
-import { NavItem } from "@/types";
+import {NavItem} from "@/types";
 import Link from "next/link";
-import { FC } from "react";
+import React, {Dispatch, FC} from "react";
 
-const NavigationLink: FC<NavItem> = ({ ...props }) => {
-  return (
-    <Link key={props.href} href={props.href} className="px-2 py-1 hover:bg-foreground transform transition-all rounded-full hover:text-black">
-      {props.label}
-    </Link>
-  );
+
+interface NavigationLinkProps extends NavItem {
+    page: string;
+    setPage: Dispatch<React.SetStateAction<string>>;
+}
+
+const NavigationLink: FC<NavigationLinkProps> = ({...props}) => {
+    return (
+        <Link key={props.href} href={props.href}
+              className={`px-2 py-1  transform transition-all rounded-full   ${props.page === props.href ? "bg-foreground text-black" : ""}`}
+              onClick={() => props.setPage(props.href)}>
+            {props.label}
+        </Link>
+    );
 };
 
 export default NavigationLink;
