a5e310fd228e9e4cef9aa6eaf5d16096e887c9a9
roni <codingpro0981@gmail.com>
2025-12-04 20:58:24 +0530

    Refactor: `Logo` component with new height and width

--- app/_components/Logo.tsx
+++ app/_components/Logo.tsx
@@ -1,11 +1,13 @@
 import Image from "next/image";
 
 const Logo = () => {
-  return (
-    <div className="logo">
-        <Image alt="logo" src="/images/zenco_logo.png" width={300} height={60}/>
-    </div>
-  );
+    return (
+        <div className="logo">
+            <div>
+                <Image alt="logo" src="/images/zenco_logo.png" width={200} height={120} className="scale-[1.299]"/>
+            </div>
+        </div>
+    );
 };
 
 export default Logo;
