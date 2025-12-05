0c142b87c1954fcbc8b4ea1f7f21226e32758796
Riddhi Manna <rddhmanna@gmail.com>
2025-12-05 19:10:47 +0530

    done for xl screen

--- app/_components/Hero.tsx
+++ app/_components/Hero.tsx
@@ -8,19 +8,25 @@ const Hero = () => {
       <div className="poster  bg-black">
         <Image src="/images/bg2.png" alt="bg" fill className="object-cover" />
       </div>
+      {/* full text section */}
       <div className="text-box center ">
         <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2  px-5  md:w-xl lg:min-w-3xl  xl:w-6xl xl:px-4 py-5 max-w-7xl">
+        {/* Word - creating brand*/}
           <div className="text-center xl:h-50 overflow-hidden ">
             <h1 className=" font-bold pt-30">
               Creating
               <HeroTextHighlight className="xl:ml-3">brand</HeroTextHighlight>
             </h1>
           </div>
-          <div className="text-left overflow-hidden relative h-20 sm:h-24  xl:h-22">
-            <h1 className="font-bold text-center lg:pr-55 lg:my-4">and digital</h1>
+          {/* word - and digital */}
+          <div className="text-center overflow-hidden relative h-20 sm:h-24 md:h-18 md:pt-2.5 lg:h-21 lg:ml-3 xl:h-22 xl:ml-3">
+            <h1 className="font-bold text-center md:pl-9 lg:pr-55 lg:py-1.5">
+              and digital
+            </h1>
           </div>
-          <div className="h-22 overflow-hidden relative mt-3 xl:h-30 text-center xl:mt-2 sm:h-24 md:h-28">
-            <h1 className="xl:mt-0 xl:absolute xl:left-2/5 xl:ml-17">
+          {/* word - solutions */}
+          <div className="h-22 overflow-hidden relative text-center  xl:h-30  sm:h-24 md:h-28">
+            <h1 className="absolute left-2/5 lg:ml-7 xl:left-2/5 xl:ml-17">
               <HeroTextHighlight>Solutions</HeroTextHighlight>
             </h1>
           </div>
